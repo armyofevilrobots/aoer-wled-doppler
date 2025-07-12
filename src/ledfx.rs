99480e8b6f34e846af23d0478899f0dc14ea2254
Derek Anderson <derek@armyofevilrobots.com>
2025-07-11 21:31:32 -0700

    Add missing deps for system tray

--- src/ledfx.rs
+++ src/ledfx.rs
@@ -1,4 +1,3 @@
-
 pub fn playpause(baseurl: &str, state: bool) -> Result<(), ureq::Error> {
     let url = format!("{baseurl}/api/virtuals");
     let result: serde_json::Value = ureq::get(url.as_str()).call()?.into_json()?;
@@ -18,10 +17,10 @@ mod tests {
     use super::*;
     use std::{thread::sleep, time::Duration};
 
-    #[test]
-    fn test_playpause() {
-        playpause("http://localhost:8888", true);
-        sleep(Duration::from_secs(3));
-        playpause("http://localhost:8888", false);
-    }
+    // #[test]
+    // fn test_playpause() {
+    //     playpause("http://localhost:8888", true);
+    //     sleep(Duration::from_secs(3));
+    //     playpause("http://localhost:8888", false);
+    // }
 }
